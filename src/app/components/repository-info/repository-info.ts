56699e991411da208f20f7bb18c5a5a8272b90ad
kaio.dos.s.valls <kaio.svalls@gmail.com>
2025-10-02 16:53:12 -0300

    Repository Info HTML added
    Component repository info HTML added and respond to object fields.

--- src/app/components/repository-info/repository-info.ts
+++ src/app/components/repository-info/repository-info.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { LucideAngularModule } from "lucide-angular";
 
 @Component({
   selector: 'app-repository-info',
-  imports: [],
+  imports: [
+    LucideAngularModule,
+    CommonModule
+  ],
   templateUrl: './repository-info.html',
   styleUrl: './repository-info.scss'
 })
 export class RepositoryInfo {
 
   @Input() selectedRepository!:any;
+  commitHystory:any[] = [
+    {message: 'fix(payment): handle null pointer', author: 'Kaio', date: '2025-09-18'},
+    {message: 'feat(api): add retry', author: 'Kaio', date: '2025-09-18'}
+  ];
 
+  gitChangeList:any[] = [
+    {path: 'src/main/java/com/example/PaymentProcessor.java', type: 'Java', status: 'modified'},
+    {path: 'src/main/resources/application.yml', type: 'YAML', status: 'added'},
+    {path: 'src/test/java/com/example/PaymentProcessorTest.java', type: 'Java', status: 'modified'},
+  ];
 }
